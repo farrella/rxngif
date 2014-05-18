64f4464efd15407489def7ef2010097e5125c33e
Abby Farrell <abbykass@gmail.com>
2014-05-18 14:00:10 -0500

    hard coded create static data

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   def show    
   	@picture = Picture.find_by({ :id => params[:id] })
   end
-end
+
 
   def index    
   	@list_of_pictures = Picture.all
@@ -12,4 +12,11 @@ end
   end
 
     def create    
+    	p = Picture.new
+    	p.caption = ({ :caption => params[:caption] })
+    	p.source = "http://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    	p.save
+
   end
+
+end
